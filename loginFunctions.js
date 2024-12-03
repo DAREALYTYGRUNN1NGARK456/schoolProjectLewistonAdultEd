5be78a37eb384d8b356c4f5727861e7cdfc2ad1a
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2024-12-03 12:49:03 -0500

    Create loginFunctions.js

---
+++ loginFunctions.js
@@ -0,0 +1,63 @@
+// Login Function \\
+function Login() {
+  var username = req.body.username;
+  var password = req.body.password;
+  db.list().then(keys => {
+    if(keys.includes(username)){
+      db.get(username).then(value => {
+        if(password == value){
+          res.cookie("loggedIn", "true");
+          res.cookie("username", username);
+          console.log("logged in successfully")
+          res.redirect("/");
+        } else{
+          res.send("Wrong password.");
+        }
+      });
+    } else{
+      res.send("Account not found.");
+    }
+  });
+}
+
+// Signup Function \\
+function Signup() {
+ var newusername = req.body.newusername;
+  newpassword = req.body.newpassword;
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  cap_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  allchars = letters + cap_letters + numbers + ['_'];
+  goodusername = true;
+  for(let i of newusername){
+    if(!allchars.includes(i)){
+      goodusername = false;
+    }
+  }
+  if(goodusername){
+    db.list().then(keys => {
+      if(keys.includes(newusername)){
+        res.send("Username taken.");
+      } else if(newusername == ""){
+        res.send("Please enter a username.");
+      } else if(newpassword == ""){
+        res.send("Please enter a password.")
+      } else{
+        db.set(newusername, newpassword).then(() => console.log("new account created"));
+        res.cookie("loggedIn", "true")
+        res.cookie("username", newusername);
+        res.redirect("/");
+      }
+    });
+  } else{
+    res.send("Username can only contain alphanumeric characters and underscores.")
+  }
+}
+
+// Logout Function \\
+function Logout() {
+ res.cookie("loggedIn", "false");
+  res.clearCookie("username");
+  res.redirect("/");
+  console.log("successfully logged out")
+}
